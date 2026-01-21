422c0dd5263d4e5d43a0366fb4d7e0339a10fe09
Uttam <uttamrohit4545@gmail.com>
2026-01-21 17:17:13 +0600

    feat : server create and configuration

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import app from "./app"
+import { prisma } from "./lib/prisma"
+
+const PORT = process.env.PORT || 5000
+
+async function  main() {
+
+    try{
+        await prisma.$connect()
+        console.log("Database connected successfully")
+
+        app.listen(PORT, () => {
+            console.log(`server is run on port ${PORT}`)
+        })
+
+    }catch(error){
+        console.error("an error occured", error)
+        await prisma.$disconnect()
+        process.exit(1)
+    }
+
+}
+
+main()
