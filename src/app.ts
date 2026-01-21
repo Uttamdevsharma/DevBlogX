421c61d089471d649620e8b2d14a5a7ddbe7ae98
Uttam <uttamrohit4545@gmail.com>
2026-01-21 18:35:57 +0600

    fEAT : user register using betterauth

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import postRouter from './post/post.router'
-
+import { toNodeHandler } from "better-auth/node";
+import { auth } from './lib/auth';
 const app = express()
 
 
 app.use(express.json())
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use('/posts',postRouter)
 
