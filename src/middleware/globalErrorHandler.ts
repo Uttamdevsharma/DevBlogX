2564946450cb6dd063ef43397c55979975e64c06
Uttam <uttamrohit4545@gmail.com>
2026-02-06 22:19:13 +0600

    feat : prismaClientValidation Error

---
+++ src/middleware/globalErrorHandler.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import { Prisma } from "../../generated/prisma/client";
+
+
+function errorHandler (
+    err: any,
+    req :Request,
+    res : Response,
+    next : NextFunction
+
+){
+
+    let statusCode = 500;
+    let errorMessage = "Internal Server Error";
+    let errorDetails = err;
+
+    //PrismaClientValidationError
+    if(err instanceof Prisma.PrismaClientValidationError){
+        statusCode = 400;
+        errorMessage = "You provide incorrect field type or missing fields!"
+    }
+
+
+    res.status(statusCode)
+    res.json({
+        message : errorMessage,
+        error : errorDetails
+    })
+
+}
